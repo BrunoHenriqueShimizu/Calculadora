f8fd3c49b3cf5be623e6185ae4ede8accb769d58
BrunoHenriqueShimizu <126803589+BrunoHenriqueShimizu@users.noreply.github.com>
2023-08-18 08:49:08 -0300

    Update calc.c
    Esse é o menu da calculadora

--- calc.c
+++ calc.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
-    //asfasfasf
+    char variavel;
+    float valor1, valor2;
+
+
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("-----------------------------");
+    printf("\n=========Bem-vindo!==========\n");
+    printf("-----------------------------");
+
+    printf("\n\nDigite a operação que deseja realizar utilizando este formato (x + y), com essas variáveis --> (+,-,*,/)\n");
+    scanf("%f %c %f", &valor1, &variavel, &valor2);
+
+
+
+    return 0;
 }
